c27ba7c6a1cf21bcb612edc957830b187a17dd4f
Gabriel Gomes <gggabriel368@gmail.com>
2026-07-08 18:31:51 -0300

    add routes from documents

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Document;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DocumentController extends Controller
 {
-    //
+    public function getAll(){
+        $array['error'] = false;
+
+        $documents = Document::all();
+        foreach($documents as $documentKey => $documentValue){
+            $documents[$documentKey]['fileurl'] = asset('storage/'.$documentValue['fileurl']);
+        }
+
+        $array['documents'] = $documents;
+
+        return response()->json($array);
+    }
 }
