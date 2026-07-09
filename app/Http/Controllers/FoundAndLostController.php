613bee1152912f81d29d9de665e520cac998e324
Gabriel Gomes <gggabriel368@gmail.com>
2026-07-09 20:59:58 -0300

    found and lost get route ok

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
 
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll(){
+        $array['error'] = false;
+
+        $lost = FoundAndLost::where('status', 'LOST')->get();
+        $found = FoundAndLost::where('status', 'FOUND')->get();
+
+        $array['found'] = $found;
+        $array['lost'] = $lost;
+
+        return response()->json($array);
+    }
 }
