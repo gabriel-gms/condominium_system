edee7b426ca708353d3688912c1370cc7d6a2c3b
Gabriel Gomes <gggabriel368@gmail.com>
2026-07-09 21:22:28 -0300

    found and lost post route ok

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FoundAndLostController extends Controller
 {
@@ -17,5 +18,32 @@ class FoundAndLostController extends Controller
         $array['lost'] = $lost;
 
         return response()->json($array);
+    }
+
+    public function setFoundAndLost(Request $request){
+        $validator = Validator::make($request->all(), [
+            'where' => 'required',
+            'description' => 'required',
+            'photos' => 'file|mimes:jpg,png'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'error' => true,
+                'message' => $validator->errors()->first()
+            ]);
+        }
+
+        $foundAndLost = new FoundAndLost();
+        $foundAndLost->where = $request->input('where');
+        $foundAndLost->description = $request->input('description');
+        $foundAndLost->created_at = date('y-m-d');
+        $foundAndLost->photos = $request->input('photos');
+        $foundAndLost->save();
+
+        return response()->json([
+            'error' => false,
+            'losts' => $foundAndLost
+        ]);
     }
 }
