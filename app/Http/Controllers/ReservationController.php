fb683c998afaae4fd5c8bfe522dc9d385841aafd
Gabriel Gomes <gggabriel368@gmail.com>
2026-07-15 20:26:18 -0300

    get reservation's areas

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Area;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function getResevations(){
+        $reservations = Area::where('allowed', 1)->get();
+
+        return response()->json([
+            'error' => false,
+            'reservations' => $reservations  
+        ]);
+    }
 }
