d955f4921e966cd98982e6c287551640db75f3f1
Gabriel Gomes <gggabriel368@gmail.com>
2026-07-20 20:12:12 -0300

    improving reservation controller

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -10,11 +10,34 @@ use Illuminate\Support\Facades\Validator;
 class ReservationController extends Controller
 {
     public function getReservations(){
-        $reservations = Area::where('allowed', 1)->get();
+        $areas = Area::where('allowed', 1)->get();
+
+        $days_helper = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'];
+    
+        foreach($areas as $area){
+            $allowed_days = explode(',', $area['days']);
+            $start_day = intval(current($allowed_days));
+            $available_days = [];
+            $available_days[] = $days_helper[$start_day];
+            array_shift($allowed_days);
+            
+            for($i = 0; $i<count($allowed_days) - 1; $i++){
+                $reload_sequence = false;
+                if(intval($allowed_days[$i]) + 1 !== intval($allowed_days[$i + 1])){
+                    $available_days[] = $days_helper[$allowed_days[$i]];
+                    $reload_sequence = true;
+                    if($reload_sequence) $available_days[] = $days_helper[$allowed_days[$i+1]];
+                } else {
+                    $available_days[] = $days_helper[$allowed_days[$i]];
+                }
+            }
+            $available_days[] = $days_helper[end($allowed_days)];
+            echo "Area ".$area->title."\n";
+            print_r($available_days);
+        }
 
         return response()->json([
-            'error' => false,
-            'reservations' => $reservations  
+            'error' => false 
         ]);
     }
 
@@ -32,26 +55,36 @@ class ReservationController extends Controller
             ]);
         }
 
-        $unit = Unit::find('id', $id_unit);
-        $area = Area::find('id', $request->input('id_area'));
+        $unit = Unit::find($id_unit);
+        $area = Area::find($request->input('id_area'));
 
         if($unit && $area){
             $start = $request->input('start');
             $end = $request->input('end');
             $date = $request->input('date');
 
-            $week_day = date('w', strtotime($date));
-            $allow_day = explode(',', $area['days']);
-            if(){
-
+            $week_day = date('w', $date);
+            $allow_days = explode(',', $area['days']);
+            if(in_array($week_day, $allow_days)){
+                if($start < $end && $end < $area['end_time'] - 1 && $start >= $area['start_time']){
+                    
+                } else {
+                    return response()->json([
+                        'error' => true,
+                        'message' => 'incorrect hour'
+                    ]);
+                }
             } else {
-                return
+                return response()->json([
+                    'error' => true,
+                    'message' => 'your day is unavailable'
+                ]);
             }
         } else {
             return response()->json([
                 'error' => true,
                 'message' => 'unit or area dont exists'
-            ])
+            ]);
         }
     }
 }
